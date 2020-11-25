bf7b6b9b28b6ab1bb70340e62162426074a5fb6d
Ray Lam <ray.lam@oocl.com>
2020-11-25 14:49:00 +0800

    finished pra3

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,18 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    private int schoolClass;
+    public Student(String name, int age, int schClass) {
+        super(name, age);
+        this.schoolClass = schClass;
+    }
+
+    public int getKlass() {
+        return schoolClass;
+    }
+    @Override
+    public String introduce(){
+        return "I am a Student. I am at Class "+ getKlass()+".";
+    }
+
 }
