368bbd656f04c318fd3e892df9b8a9c1d24b37a0
Ray Lam <ray.lam@oocl.com>
2020-11-25 19:43:44 +0800

    finished pra9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
+
+import java.util.ArrayList;
 public class Klass {
+    private int classNum;
+    private Student leader;
+    private ArrayList<Student> studentList;
+
+    public Klass(int classNum) {
+        this.classNum = classNum;
+        leader = null;
+        studentList = new ArrayList<>();
+    }
+
+    public int getNumber() {
+        return classNum;
+    }
+
+    public String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+    public void assignLeader(Student leader) {
+        if (!this.studentList.contains(leader)) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void appendMember(Student student) {
+        studentList.add(student);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,56 @@
 package practice09;
 
-public class Teacher {
+
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person{
+    private Klass klass;
+    private LinkedList<Klass> klassList = new LinkedList<>();
+
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> klassList) {
+        super(id, name, age);
+        this.klass = klass;
+        this.klassList = klassList;
+    }
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        this.klass = null;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (getClasses().size() > 0) {
+            String myClasses = getClasses().stream().map(classNum -> Integer.toString(classNum.getNumber())).collect(Collectors.joining(", "));
+            return String.format("%s I am a Teacher. I teach Class %s.", super.introduce(), myClasses);
+        }
+        return String.format("%s I am a Teacher. I teach No Class.", super.introduce());
+    }
+
+    public String introduceWith(Student jerry) {
+        if (getClasses().contains(jerry.getKlass())){
+            return String.format("%s I am a Teacher. I teach %s.", super.introduce(), jerry.getName());
+        }
+        return String.format("%s I am a Teacher. I don't teach %s.", super.introduce(), jerry.getName());
+
+    }
+    public LinkedList<Klass> getClasses(){
+        return this.klassList;
+    }
+
+    public Boolean isTeaching(Student student){
+        if(getClasses().contains(student.getKlass())){
+            return true;
+        }
+        return false;
+    }
 }
