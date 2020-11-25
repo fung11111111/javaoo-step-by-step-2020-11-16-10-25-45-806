b595736abb1b945e0f2ddf6570aa784df4591920
Ray Lam <ray.lam@oocl.com>
2020-11-25 14:59:59 +0800

    finished pra5

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,17 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+    private int schoolClass;
+    public Student(String name, int age, int schClass) {
+        super(name, age);
+        this.schoolClass = schClass;
+    }
+
+    public int getKlass() {
+        return schoolClass;
+    }
+    @Override
+    public String introduce(){
+        return "My name is "+getName()+". I am "+ getAge()+" years old. I am a Student. I am at Class "+getKlass()+".";
+    }
 }
--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,32 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int schoolClass;
+    private Boolean noClass;
+    public Teacher(String name, int age, int schClass) {
+        super(name, age);
+        this.schoolClass = schClass;
+        this.noClass = false;
+    }
+    public Teacher(String name, int age){
+        super(name, age);
+        this.noClass = true;
+    }
+
+    public int getKlass() {
+        return schoolClass;
+    }
+
+    public Boolean getNoClass() {
+        return noClass;
+    }
+    @Override
+    public String introduce(){
+        if(getNoClass()){
+            return "My name is "+getName()+". I am "+ getAge()+" years old. I am a Teacher. I teach No Class.";
+        }else{
+            return "My name is "+getName()+". I am "+ getAge()+" years old. I am a Teacher. I teach Class "+getKlass()+".";
+        }
+
+    }
 }
